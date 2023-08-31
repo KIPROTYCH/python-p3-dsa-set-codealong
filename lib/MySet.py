fb8f19d09ce3abdc3c215814b0b4fa2fe6ac9b01
KIPROTYCH <kiprotych@gmail.com>
2023-08-31 13:42:54 +0300

    Completed Assignment

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,61 @@
-class MySet:
+class MySet:# Define new class named MySet
+    # Defines the constructor method of the class which takes an optional argument initial_data that defaults to None.
+    def __init__(self, initial_data=None):
+        self.dictionary = {}
+        if initial_data:
+            for item in initial_data:
+                self.dictionary[item] = True
 
-    pass
+    def add(self, item):
+        self.dictionary[item] = True
+
+    def delete(self, item):
+        if item in self.dictionary:
+            del self.dictionary[item]
+
+    def has(self, item):
+        return item in self.dictionary
+
+    def size(self):
+        return len(self.dictionary)
+
+
+    def __init__(self, initial_data=None):
+        self.dictionary = {}
+        if initial_data:
+            for item in initial_data:
+                self.dictionary[item] = True
+
+    def add(self, item):
+        self.dictionary[item] = True
+
+    def delete(self, item):
+        if item in self.dictionary:
+            del self.dictionary[item]
+
+    def has(self, item):
+        return item in self.dictionary
+
+    def size(self):
+        return len(self.dictionary)
+
+
+    def __init__(self, initial_data=None):
+        self.dictionary = {}
+        if initial_data:
+            for item in initial_data:
+                self.dictionary[item] = True
+
+    def add(self, item):
+        self.dictionary[item] = True
+
+    def delete(self, item):
+        if item in self.dictionary:
+            del self.dictionary[item]
+
+    def has(self, item):
+        return item in self.dictionary
+
+    def size(self):
+        return len(self.dictionary)
+
